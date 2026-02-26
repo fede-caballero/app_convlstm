2ca389392afc406991306f482ad2ae7366f1d8b3
fede-caballero <f.caballero@alumno.um.edu.ar>
2026-02-26 14:52:38 -0300

    Changed colours of tabs

--- frontend/components/tutorial-dialog.tsx
+++ frontend/components/tutorial-dialog.tsx
@@ -82,12 +82,12 @@ export function TutorialDialog({ open, onOpenChange }: TutorialDialogProps) {
                     </DialogDescription>
                 </DialogHeader>
                 <Tabs defaultValue="features" className="w-full">
-                    <TabsList className="grid w-full grid-cols-2 bg-white/10 mb-2">
+                    <TabsList className="grid w-full grid-cols-2 bg-orange-400 mb-2">
                         <TabsTrigger value="features" className="data-[state=active]:bg-primary data-[state=active]:text-primary-foreground">
                             <Info className="w-4 h-4 mr-2" />
                             {t("Funcionalidades", "Features")}
                         </TabsTrigger>
-                        <TabsTrigger value="scale" className="data-[state=active]:bg-primary data-[state=active]:text-primary-foreground">
+                        <TabsTrigger value="scale" className="data-[state=active]:bg-orange-400 data-[state=active]:text-primary-foreground">
                             <Cloud className="w-4 h-4 mr-2" />
                             {t("Escala de Radar", "Radar Scale")}
                         </TabsTrigger>
